5f25415dcb870aff844e626f660059a8ef05f7d2
JMcB17 <joel.mcbride1@live.com>
2020-09-12 16:34:15 +0100

    Fix typo
    made a typo

--- flask-test.py
+++ flask-test.py
@@ -1,6 +1,9 @@
 from flask import Flask
-app = Flas(__name__)
+app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "hello world"
+
+# if __name__ == '__main__':
+#     app.run()
