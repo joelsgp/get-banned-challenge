d7ca21f559879a523ffe7d4767dbca4f655cba8b
Joel McBride <joel.mcbride1@live.com>
2020-09-12 16:58:56 +0100

    Add bit to test IP address checking

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "hello world"
+    return "hello world {}".format(request.remote_addr)
 
 if __name__ == '__main__':
     app.run()
