338a32d666c95dba94fe22c07029910067e376c7
Joel McBride <joel.mcbride1@live.com>
2020-09-13 16:22:04 +0100

    Revert main branch to stable, with new dev branch for saving changes. lol. Also rename master branch to main because BLM

--- app.py
+++ app.py
@@ -19,33 +19,33 @@ def hello_world():
     # interval_seconds = 21600
     interval_seconds = interval_hours * (60^2)
 
-    # Connect to PostgreSQL database
-    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-    cur = conn.cursor()
-
-    # Example code
-    try:
-        cur.execute("SELECT access-time FROM recent-ips WHERE ip=%s",
-                    (request_ip,))
-    except ProgrammingError:
-        cur.execute("""
-                    INSERT INTO recent-ips(ip, access-time)
-                    VALUES(%s, %s)
-                    """,
-                    (request_ip, time()))
-
-    # If IP is on record, check if it accessed less than 6 hours ago
-    if request_ip in recent_ips:
-        if recent_ips[request_ip] > time()-interval_seconds:
-            request_interval_seconds = time()-recent_ips[request_ip]
-            request_interval_hours = request_interval_seconds / (60^2)
-            return """
-                   IP duplication error: you already requested words
-                   {} hour(s) ago! Please ensure you wait at least " \
-                   "{} hours before requesting new words." \
-                   .format(recent_request_interval_hours, interval_hours)
-        else:
-            recent_ips[request_ip] = time()
+##    # Connect to PostgreSQL database
+##    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+##    cur = conn.cursor()
+##
+##    # Example code
+##    try:
+##        cur.execute("SELECT access-time FROM recent-ips WHERE ip=%s",
+##                    (request_ip,))
+##    except ProgrammingError:
+##        cur.execute("""
+##                    INSERT INTO recent-ips(ip, access-time)
+##                    VALUES(%s, %s)
+##                    """,
+##                    (request_ip, time()))
+##
+##    # If IP is on record, check if it accessed less than 6 hours ago
+##    if request_ip in recent_ips:
+##        if recent_ips[request_ip] > time()-interval_seconds:
+##            request_interval_seconds = time()-recent_ips[request_ip]
+##            request_interval_hours = request_interval_seconds / (60^2)
+##            return """
+##                   IP duplication error: you already requested words
+##                   {} hour(s) ago! Please ensure you wait at least " \
+##                   "{} hours before requesting new words." \
+##                   .format(recent_request_interval_hours, interval_hours)
+##        else:
+##            recent_ips[request_ip] = time()
     
     return "hello world {}".format(request_ip)
 
