6e94778131aac9de7637c32ff9e5ee64cb8a135e
Joel McBride <joel.mcbride1@live.com>
2020-09-16 20:59:00 +0100

    Set the required interval to the real one, ready for beta.

--- app.py
+++ app.py
@@ -16,8 +16,8 @@ app = Flask(__name__)
 
 DATABASE_URL = os.environ["DATABASE_URL"]
 # This is the enforced interval between providing new words.
-##INTERVAL_HOURS = 6
-INTERVAL_HOURS = 0.005
+INTERVAL_HOURS = 6
+##INTERVAL_HOURS = 0.005
 # This determines whether the app will tell the user the progress
 # through the words.
 # I think I'll keep it turned off at the start because it will be more
