f965e8bd132f5bc16e23f7c68c289b35521500b9
Joel McBride <joel.mcbride1@live.com>
2020-09-16 18:23:33 +0100

    Add file to be used when the word is found, to get the most recently served messages. Also, add missing fullstop to a comment in another file.

--- get_recent_ips_last_messages.py
+++ get_recent_ips_last_messages.py
@@ -1,23 +1,18 @@
 import psycopg2
-
 from jmcb_postgresql import postgresql_connect, postgresql_disconnect
 
 # Connect to database.
 conn, cur = postgresql_connect()
 
-
-# Get the 10 longest words in the database.
+# Get the 10 most recent IP addresses that connected.
+# This will include the messages they were served.
 cur.execute("""
-            SELECT * FROM wordlist
-            ORDER BY CHAR_LENGTH(word) DESC
+            SELECT * FROM recent_ips
+            SORT BY access_time DESC
             LIMIT 10
             """)
-
-# Print 'em out.
-for sql_response in cur.fetchall():
-    word = sql_response[1]
-    print("{} long: {}".format(len(word), word))
-
+# laziness
+print(cur.fetchall())
 
 # Disconnect from server.
 postgresql_disconnect(conn, cur)
