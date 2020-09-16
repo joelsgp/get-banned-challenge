fad4e38353f5864810b6ad76db273c81ee4a266c
Joel McBride <joel.mcbride1@live.com>
2020-09-16 19:26:33 +0100

    Add better formatting to response for this script.

--- get_recent_ips_last_messages.py
+++ get_recent_ips_last_messages.py
@@ -9,11 +9,16 @@ conn, cur = postgresql_connect()
 # This will include the messages they were served.
 cur.execute("""
             SELECT * FROM recent_ips
-            SORT BY access_time DESC
+            ORDER BY access_time DESC
             LIMIT 10
             """)
-# laziness
-print(cur.fetchall())
+
+# Print 'em out.
+for sql_response in cur.fetchall():
+    ip = sql_response[0]
+    access_time = sql_response[1]
+    message = sql_response[2]
+    print("IP {}, access time {}:\n{}".format(ip, access_time, message))
 
 # Disconnect from server.
 postgresql_disconnect(conn, cur)
